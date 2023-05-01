8a86ee23e213d6bfaa56dd60a458aa6d63552ec2
Anthony Diggle <fuseinabowl@gmail.com>
2023-05-01 15:05:40 +0100

    Update the towns fed UI from the TownStatuses script

--- Assets/Scripts/Util/TownStatuses.cs
+++ Assets/Scripts/Util/TownStatuses.cs
@@ -1,11 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TownStatuses : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Supports {0} for total towns and {1} for fed towns")]
+    private string formatString;
+
+    [SerializeField]
+    private TextMeshProUGUI hudText;
+
     private Dictionary<Station, bool> townIsFed = new Dictionary<Station, bool>();
+
+    private void Start()
+    {
+        UpdateHud();
+    }
 
     public void RegisterTown(Station town)
     {
@@ -15,5 +30,15 @@ public class TownStatuses : MonoBehaviour
     public void OnTownIsFedChanged(Station town, bool isFed)
     {
         townIsFed[town] = isFed;
+
+        UpdateHud();
+    }
+
+    private void UpdateHud()
+    {
+        var totalTowns = townIsFed.Count;
+        var fedTowns = townIsFed.Count(townRecord => townRecord.Value);
+
+        hudText.text = String.Format(formatString, totalTowns, fedTowns);
     }
 }
